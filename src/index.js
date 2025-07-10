0fe9ded53666a4984848af7ea124f54ea25bd6b6
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-10 18:41:42 +0700

    update database and server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import studentRoutes from './routes/student.routes.js';
 import courseRoutes from './routes/course.routes.js';
 import teacherRoutes from './routes/teacher.routes.js';
+import userRoutes from './routes/auth.routes.js';
 import { serveSwagger, setupSwagger } from './config/swagger.js';
+import { authToken } from './middleware/auth.js';
 
 dotenv.config();
 const app = express();
@@ -11,11 +14,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(cors({
+  origin: "*"
+}));
+
 app.use('/docs', serveSwagger, setupSwagger);
 
-app.use('/students', studentRoutes);
-app.use('/courses', courseRoutes);
-app.use('/teachers', teacherRoutes);
+app.use('/students', authToken, studentRoutes);
+app.use('/courses', authToken, courseRoutes);
+app.use('/teachers', authToken, teacherRoutes);
+app.use('/auth', userRoutes);
 
 app.get('/', (req, res) => res.send('Welcome to School API!'));
 
